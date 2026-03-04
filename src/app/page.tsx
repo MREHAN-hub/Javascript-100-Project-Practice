5138babbbb564cace17cc051e4867c86eadbce8a
Muhammad Rehan <147375563+MREHAN-hub@users.noreply.github.com>
2026-03-04 19:07:05 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import FAQ from "@/components/FAQ";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      {/* header */}
+      <Header />
+      {/* main  */}
+      <main>
+        <Hero />
+        <FAQ />
+        <Footer />
+      </main>
+
+    </div>
+  );
+}
